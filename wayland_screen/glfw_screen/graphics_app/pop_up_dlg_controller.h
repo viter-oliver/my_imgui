b4619ab9bb79bdd8522fe6ae74a6341e67c39487
viter <zhangjiusheng@auto-future.com>
2019-02-27 18:15:44 +0800

    del

--- wayland_screen/glfw_screen/graphics_app/pop_up_dlg_controller.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "afg.h"
-class pop_up_dlg_controller
-{
-public:
-	pop_up_dlg_controller(base_ui_component* root);
-	~pop_up_dlg_controller();
-};
-
--- wayland_screen/glfw_screen/imgui/README.md
+++
@@ -1,3 +0,0 @@
-my_imgui,
-=====
-my_imgui is a graphics-design tool based on imgui 
